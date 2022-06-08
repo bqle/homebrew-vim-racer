69ce871ef6d593e8416a9be8c9b225eb5d43b2a7
bqle <benjaminle2002@gmail.com>
2022-06-08 10:41:04 -0400

    v0.2.0

--- Formula/vim-racer.rb
+++ Formula/vim-racer.rb
@@ -1,11 +1,11 @@
 class VimRacer < Formula
 	desc "CLI for practicing Vim"
 	homepage "https://github.com/bqle/vim-racer"
-	url "https://github.com/bqle/vim-racer/releases/download/v0.1.0/vim-racer.tar.gz"
-	sha256 "b97ab8d4a29a13fc764af7370f4d699fd61a9ab458896c14dc89b74732d0ed4c"
-	version "0.1.0"
+	url "https://github.com/bqle/vim-racer/releases/download/v0.2.0/vim-racer.tar.gz"
+	sha256 "6d9f8fb460c9c5aa49595fcc25960fd925610f02c9b2de83b6ff75db5f45da0b"
+	version "0.2.0"
 	
 	def install
-		bin.install "out"
+		bin.install "vim-racer"
 	end
 end
