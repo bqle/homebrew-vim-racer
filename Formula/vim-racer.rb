a14d838298c7b0d9608a9b887a88bf9cf6b227fb
bqle <benjaminle2002@gmail.com>
2022-06-08 09:27:30 -0400

    first release

---
+++ Formula/vim-racer.rb
@@ -0,0 +1,11 @@
+class VimRacer<
+	desc "CLI for practicing Vim"
+	homepage "https://github.com/bqle/vim-racer"
+	url "https://github.com/bqle/vim-racer/releases/download/v0.1.0/vim-racer.tar.gz"
+	sha256 "b97ab8d4a29a13fc764af7370f4d699fd61a9ab458896c14dc89b74732d0ed4c"
+	version "0.1.0"
+	
+	def install
+		bin install "out"
+	end
+end
